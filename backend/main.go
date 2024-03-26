1ff83585d2887e504115600a61ac0eccb506618b
dhg <hg.duan@yamu.com>
2024-03-26 14:04:39 +0800

    backend

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"main.go/serve"
 )
 
 func main() {
 	fmt.Println("后端项目启动")
-	server := serve.NewServer("127.0.0.1", 8888)
+	server := NewServer("127.0.0.1", 8888)
 	server.Start()
 }
